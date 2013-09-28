03ec1af4ccad3a182846898bf6ac7ba9b69d5deb
steve <stephan.friedl.github@gmail.com>
2013-09-27 18:18:53 -0600

    Cleanup

--- Utility/src/Utility/Logging.cpp
+++ Utility/src/Utility/Logging.cpp
@@ -1,9 +1,28 @@
 /*
- * Logging.cpp
- *
- *  Created on: Sep 17, 2013
- *      Author: steve
- */
+Copyright (c) 2013 Stephan Friedl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+Except as contained in this notice, the name(s) of the above copyright holders
+shall not be used in advertising or otherwise to promote the sale, use or other
+dealings in this Software without prior written authorization.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 
 #include "Logging.h"
@@ -14,13 +33,20 @@
 #include <boost/log/sources/record_ostream.hpp>
 
 #include <boost/log/expressions.hpp>
-#include <boost/log/utility/setup/console.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/support/date_time.hpp>
+
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/settings.hpp>
+#include <boost/log/utility/setup/from_settings.hpp>
 
 #include <boost/ptr_container/ptr_list.hpp>
 
+#include <boost/property_tree/ptree.hpp>
+
 #include <boost/atomic.hpp>
+
 
 
 
@@ -269,31 +295,191 @@ namespace SEFUtility
     }
 
 
-	typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> 	LoggingSink;
 
 
-	void		Logging::Init()
+	class BoostLogManager
 	{
-		boost::shared_ptr<LoggingSink>	consoleSink = boost::log::add_console_log();
+	public :
 
-		boost::log::formatter formatter =
-			boost::log::expressions::stream
-				<< boost::log::expressions::attr< unsigned int >("LineID") << ": "
-				<< boost::log::expressions::format_date_time< boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d %H:%M:%S") << "  "
-				<< boost::log::expressions::attr< std::string >( "Channel" )
-				<< severity
-				<< boost::log::expressions::message;
+		typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> 	LoggingSink;
+		typedef	boost::log::sinks::synchronous_sink<boost::log::sinks::text_file_backend>		FileSink;
 
-		consoleSink->set_formatter( formatter );
-		consoleSink->locked_backend()->auto_flush(true);
 
-//		boost::log::core::get()->set_filter( severity >= LoggingSeverityLevels::info );
+		BoostLogManager()
+		{
+			//	Create the console and file sinks.  The file sink is just a text file named 'startup' with a count suffix.
 
-		boost::log::add_common_attributes();
+			boost::shared_ptr<LoggingSink>	consoleSink = boost::log::add_console_log();
+			boost::shared_ptr<FileSink>		fileSink = boost::log::add_file_log("startup_%N.log");
+
+			//	Set the formatter for the console and text file to the default and set autoflush as well.
+
+			boost::log::formatter defaultFormatter =
+				boost::log::expressions::stream
+					<< boost::log::expressions::attr< unsigned int >("LineID") << ": "
+					<< boost::log::expressions::format_date_time< boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d %H:%M:%S") << "  "
+					<< boost::log::expressions::attr< std::string >( "Channel" )
+					<< severity
+					<< boost::log::expressions::message;
+
+			consoleSink->set_formatter( defaultFormatter );
+			consoleSink->locked_backend()->auto_flush( true );
+
+			fileSink->set_formatter( defaultFormatter );
+			fileSink->locked_backend()->auto_flush( true );
+
+			//	Add the common attributes
+
+			boost::log::add_common_attributes();
+
+			//	Write the first record to the log.
+
+			Logger( "LoggingInitialization" ).NormalStream() << "Logging Initiated" << std::endl;
+		}
+
+
+
+		void		DefaultLoggingConfig( const char*			processName )
+		{
+			//	Start by stripping out all the existing sinks.  There should only be the startup console and file anyway.
+
+			boost::log::core::get()->remove_all_sinks();
+
+			//	Create the console sinks
+
+			boost::shared_ptr<LoggingSink>	consoleSink = boost::log::add_console_log();
+
+			//	Create the fileSink.  Use the process name with a counting suffix and a simple roll every 1MB rule.
+
+			boost::shared_ptr<FileSink>	fileSink( new FileSink( boost::log::keywords::file_name = std::string( processName ) + "_%N.log",
+																boost::log::keywords::rotation_size = 1048576 ));
+
+			//	Create the default formatter and assign it to both sinks
+
+			boost::log::formatter formatter =
+				boost::log::expressions::stream
+					<< boost::log::expressions::attr< unsigned int >("LineID") << ": "
+					<< boost::log::expressions::format_date_time< boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d %H:%M:%S") << "  "
+					<< boost::log::expressions::attr< std::string >( "Channel" )
+					<< severity
+					<< boost::log::expressions::message;
+
+			consoleSink->set_formatter( formatter );
+			consoleSink->locked_backend()->auto_flush( true );
+
+			fileSink->set_formatter( formatter );
+			fileSink->locked_backend()->auto_flush( true );
+
+			//	Default to only warnings and above.
+
+			boost::log::core::get()->set_filter( severity >= LoggingSeverityLevels::warning );
+
+			//	Add the common attributes
+
+			boost::log::add_common_attributes();
+
+			//	Output a warning message that the logging was not configured
+
+			Logger( "DefaultLoggingConfig" ).WarningStream() << "No logging configuration specified.  Default logging config used." << std::endl;
+		}
+
+	};
+
+
+	static BoostLogManager		g_boostLogManager;
+
+
+	void		SetIfInitialized( boost::log::settings&						settings,
+								  const std::string&						label,
+								  boost::optional<const std::string&>		value )
+	{
+		if( value.is_initialized() )
+		{
+			settings[label] = value.get();
+		}
 	}
 
 
-	void		Logging::FiltersChanged()
+	Logging::InitResult				Logging::Init( const char*								processName,
+							   	   	   	   	   	   const boost::property_tree::ptree&		loggingSection )
+	{
+		//	Logging configuration settings block
+
+		boost::log::settings 		configSettings;
+
+		configSettings["Core"]["DisableLogging"] = false;
+
+		configSettings["Sinks.Console"]["Destination"] = "Console";
+		configSettings["Sinks.Console"]["AutoFlush"] = true;
+
+		//	Get a collection of all the logging files to open
+
+		{
+			auto	filesSection = loggingSection.get_child_optional( "files" );
+
+			if( !filesSection.is_initialized() )
+			{
+				Logger( "LoggingConfigInit" ).ErrorStream() << "Missing 'files' section in logging configuration.  Using Default Config." << std::endl;
+
+				g_boostLogManager.DefaultLoggingConfig( processName );
+
+				return( InitResult::Failure( ErrorCodes::MISSING_FILES_SECTION, "Missing 'files' section in logging configuration.  Using Default Config." ) );
+			}
+
+			auto fileRange = filesSection->equal_range( "file" );
+
+			for( auto itrFileSpec = fileRange.first; itrFileSpec != fileRange.second; ++itrFileSpec )
+			{
+				LogFileSpec		fileSpec;
+
+				auto		parseResult = fileSpec.Parse( itrFileSpec->second );
+
+				if( parseResult.Failed() )
+				{
+					Logger( "LoggingConfigInit" ).ErrorStream() << "Missing 'files' section in logging configuration.  Using Default Config." << std::endl;
+
+					g_boostLogManager.DefaultLoggingConfig( processName );
+
+					return( InitResult::Failure( ErrorCodes::BAD_FILE_SPEC, "Bad File Specification.  Using Default Config." ) );
+				}
+
+				std::string		prefix = std::string( "Sinks." ) + fileSpec.sink_name();
+
+				configSettings[prefix + ".Destination"] = "TextFile";
+				configSettings[prefix + ".FileName"] = fileSpec.file_name();
+				configSettings[prefix + ".AutoFlush"] = fileSpec.auto_flush();
+
+				SetIfInitialized( configSettings, prefix + std::string( ".Target" ), fileSpec.fieldValue( "target" ) );
+				SetIfInitialized( configSettings, prefix + std::string( ".Filter" ), fileSpec.fieldValue( "filter" ) );
+				SetIfInitialized( configSettings, prefix + std::string( ".Format" ), fileSpec.fieldValue( "format" ) );
+			}
+		}
+
+		//	Start by stripping out all the existing sinks.  There should only be the startup console and file anyway.
+
+		boost::log::core::get()->remove_all_sinks();
+
+		//	Initialize from the settings we just pulled from the config
+
+		boost::log::init_from_settings( configSettings );
+
+		Logger( "LoggingConfigInit" ).NormalStream() << "Logging initialized from configuration file." << std::endl;
+
+		//	Finished with Success
+
+		return( InitResult::Success() );
+	}
+
+
+
+	void							Logging::DefaultConfiguration( const char*		processName )
+	{
+		g_boostLogManager.DefaultLoggingConfig( processName );
+	}
+
+
+
+	void							Logging::FiltersChanged()
 	{
 		g_LoggingFilterChangeCount.fetch_add( 1 );
 	}
--- Utility/src/Utility/Logging.h
+++ Utility/src/Utility/Logging.h
@@ -1,9 +1,30 @@
 /*
- * Logging.h
- *
- *  Created on: Sep 8, 2013
- *      Author: steve
- */
+Copyright (c) 2013 Stephan Friedl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+Except as contained in this notice, the name(s) of the above copyright holders
+shall not be used in advertising or otherwise to promote the sale, use or other
+dealings in this Software without prior written authorization.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
 
 #ifndef LOGGING_H_
 #define LOGGING_H_
@@ -12,6 +33,14 @@
 #include <memory>
 #include <iostream>
 #include <sstream>
+#include <ostream>
+
+#include "ConfigManager.h"
+
+#include <boost/assign/list_of.hpp>
+
+#include <boost/algorithm/string/predicate.hpp>
+
 
 
 //
@@ -144,15 +173,82 @@ namespace SEFUtility
 	{
 	public :
 
-		static void		Init();
+		enum class ErrorCodes { SUCCESS = 0,
+								MISSING_FILES_SECTION,
+								BAD_FILE_SPEC };
 
-		static void		FiltersChanged();
+		typedef Result<ErrorCodes>		InitResult;
+
+
+		static InitResult					Init( const char*								processName,
+				   	   	   	  	  	  	  	  	  const boost::property_tree::ptree&		loggingSection );
+
+		static void							DefaultConfiguration( const char*				processName );
+
+		static void							FiltersChanged();
 
 		static std::unique_ptr<ILogger>		GetLogger( const char*		channelName );
 	};
 
 
+
+	//
+	//	Initialization specification classes
+	//
+
+	class GeneralSettings : public PTreeConfigSettings
+	{
+	public :
+
+		GeneralSettings()
+			: PTreeConfigSettings( std::list<std::string>(),
+								   boost::assign::list_of( "filter" ) )
+		{}
+	};
+
+
+
+	class ConsoleSpec : public PTreeConfigSettings
+	{
+	public :
+
+		ConsoleSpec()
+			: PTreeConfigSettings( boost::assign::list_of( "sink_name" ),
+								   boost::assign::list_of( "filter" )( "format" )( "asynchronous" )( "auto_flush" ) )
+		{}
+	};
+
+
+	class LogFileSpec : public PTreeConfigSettings
+	{
+	public :
+
+
+		LogFileSpec()
+			: PTreeConfigSettings( boost::assign::list_of( "sink_name" )( "file_name" )( "auto_flush" ),
+								   boost::assign::list_of( "target" )( "rotation_size" )( "time_based_rotation" )( "filter" )( "asynchronous" ) )
+		{}
+
+
+		const std::string&		sink_name() const
+		{
+			return( requiredFieldValue( "sink_name" ) );
+		}
+
+		const std::string&		file_name() const
+		{
+			return( requiredFieldValue( "file_name" ) );
+		}
+
+		bool					auto_flush() const
+		{
+			return( boost::iequals( requiredFieldValue( "auto_flush" ), "true" ) );
+		}
+
+	};
+
 }
+
 
 
 
